3e23092fb1aede8d4ba3cfa5ff750d1c6587f48a
Chengzhi <zmq175@qq.com>
2023-05-17 03:19:10 +0800

    fix send

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ async def handleRandomSetu(bot: Bot, event: MessageEvent, state: T_State):
             img_list = gallery.img_list
             img_list = json.loads(img_list)
             for img in img_list:
-                await bot.send(message=[MessageSegment.image(img), f"这是你要的{word}福利图"])
+                await bot.send(message=Message([MessageSegment.image(img), f"这是你要的{word}福利图"]), event=event)
     else:
         await bot.send(message="这个真没有")
 
@@ -66,6 +66,6 @@ async def handleRandomSetu(bot: Bot, event: MessageEvent, state: T_State):
             img_list = gallery.img_list
             img_list = json.loads(img_list)
             for img in img_list:
-                await bot.send(message=[MessageSegment.image(img), f"这是你要的福利图"])
+                await bot.send(message=Message([MessageSegment.image(img), f"这是你要的福利图"]))
     else:
         await bot.send(message="这个真没有")
